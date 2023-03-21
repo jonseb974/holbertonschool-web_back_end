6d9cc4d2c6d3c4b3a98e2e8d7265da9fb63241a1
jonseb974 <5151@holbertonstudents.com>
2023-03-21 16:04:20 +0100

    testing arrow function

--- ES6_basic/2-arrow.js
+++ ES6_basic/2-arrow.js
@@ -1,11 +1,8 @@
-const getNeighborhoodsList = () => {
+export default function getNeighborhoodsList () {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
-  const self = this;
   this.addNeighborhood = (newNeighborhood) => {
     self.sanFranciscoNeighborhoods.push(newNeighborhood);
     return self.sanFranciscoNeighborhoods;
   };
 }
-
-export default getNeighborhoodsList;
