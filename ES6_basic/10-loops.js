9d2af3a2016d6734a1273c950eec57a111a5db41
jonseb974 <5151@holbertonstudents.com>
2023-03-22 16:34:00 +0100

    script logic update

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (var idx in array) {
-    var value = array[idx];
-    array[idx] = appendString + value;
+  let newArray = [];
+  for (let value of array) {
+    newArray.push(appendString + value);
   }
 
-  return array;
+  return newArray;
 }
