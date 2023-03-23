469b54afb80d360e1fd8c415cd9f6f644a192b79
jonseb974 <5151@holbertonstudents.com>
2023-03-23 10:50:09 +0100

    update to files_12

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -1,3 +1,20 @@
 export default function createReportObject(employeesList) {
 
+    const allEmployees = []; //Create an object to hold all employees
+
+    employeesList.forEach(employee => {
+        const { department, ...employeeData } = employee;
+        if (allEmployees.hasOwnProperty(department)) {
+            allEmployees[department] = [...allEmployees[department], employeeData];
+        } else {
+            allEmployees[department] = [employeeData];
+        }
+    });
+
+    return {
+        allEmployees,
+        getNumberOfDepartments(){
+            return Object.keys(allEmployees).length;
+        },
+    };
 }
