74729e0972db9e0e6fbf23ff8fa9c1009306cbf7
jonseb974 <5151@holbertonstudents.com>
2023-03-23 12:13:05 +0100

    fixing syntax error

--- ES6_basic/3-default-parameter.js
+++ ES6_basic/3-default-parameter.js
@@ -1,5 +1,4 @@
 export default function getSumOfHoods(initialNumber, expansion1989=89,
 expansion2019=19) {
-  (expansion1989 ? undefined : 89) || (expansion2019 ? undefined : 19);
   return initialNumber + expansion1989 + expansion2019;
 }
