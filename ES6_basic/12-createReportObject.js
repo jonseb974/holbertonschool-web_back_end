6d513e2ef863837e948298a36aad1d359523c94e
jonseb974 <5151@holbertonstudents.com>
2023-03-23 14:01:20 +0100

    fixing space error

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -1,20 +1,6 @@
 export default function createReportObject(employeesList) {
-
-    const allEmployees = []; //Create an object to hold all employees
-
-    employeesList.forEach(employee => {
-        const { department, ...employeeData } = employee;
-        if (allEmployees.hasOwnProperty(department)) {
-            allEmployees[department] = [...allEmployees[department], employeeData];
-        } else {
-            allEmployees[department] = [employeeData];
-        }
-    });
-
-    return {
-        allEmployees,
-        getNumberOfDepartments(){
-            return Object.keys(allEmployees).length;
-        },
-    };
+  return {
+    allEmployees: { ...employeesList },
+    getNumberOfDepartments: (employees) => Object.keys(employees).length,
+  };
 }
