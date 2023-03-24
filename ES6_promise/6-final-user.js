bb412f4544c5a255fa7a21a58fb7a77713da8a1a
jonseb974 <5151@holbertonstudents.com>
2023-03-24 17:26:37 +0100

    add file 6 script

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -4,7 +4,8 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const promise = signUpUser(firstName, lastName);
   const promise2 = uploadPhoto(fileName);
-  return Promise.allSettled([promise, promise2])
+  return Promise
+    .allSettled([promise, promise2])
     .then((values) => values
       .map((v) => ({ status: v.status, value: v.status === 'fulfilled' ? v.value : `${v.reason.name}:${v.reason.message}` })));
 }
