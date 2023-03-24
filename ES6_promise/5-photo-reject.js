b975ab78989032e16374293a0747ba9fbeafc8b9
jonseb974 <5151@holbertonstudents.com>
2023-03-24 16:56:16 +0100

    file5 function name

--- ES6_promise/5-photo-reject.js
+++ ES6_promise/5-photo-reject.js
@@ -1,4 +1,4 @@
-export default function signUpUser(fileName) {
+export default function uploadPhoto(fileName) {
   return new Promise((resolve, reject) => {
     const error = new Error(`${fileName}cannot be processed`);
     reject(error);
