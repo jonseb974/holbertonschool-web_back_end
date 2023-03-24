599644c49300e472cf606fcb14fc626956fb6269
jonseb974 <5151@holbertonstudents.com>
2023-03-24 16:52:21 +0100

    file5 syntax fixed

--- ES6_promise/5-photo-reject.js
+++ ES6_promise/5-photo-reject.js
@@ -1,6 +1,6 @@
 export default function signUpUser(fileName) {
   return new Promise((resolve, reject) => {
-    const error = new Error({ fileName }`cannot be processed`);
+    const error = new Error(`${fileName}cannot be processed`);
     reject(error);
   });
 }
