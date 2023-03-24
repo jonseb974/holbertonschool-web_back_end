ccdc9a278f71b730a6717acd45929bc3b30d769b
jonseb974 <5151@holbertonstudents.com>
2023-03-24 21:07:58 +0100

    typo fixed file1

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,7 +1,7 @@
 export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success) {
-      const fulfilled = { status: 200, body: 'success' };
+      const fulfilled = { status: 200, body: 'Success' };
       resolve(fulfilled);
     } else {
       const error = new Error('The fake API is not working currently');
