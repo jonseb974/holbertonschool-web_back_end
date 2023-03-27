f473e1c07ff01fd25593acf9e2ca144bbd74b8aa
jonseb974 <5151@holbertonstudents.com>
2023-03-27 18:17:18 +0200

    script test file1

---
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,9 @@
+function getListStudentIds(students) {
+  if (!Array.isArray(students)) {
+    return [];
+  }
+
+  return students.map((students) => students.id);
+}
+
+export default getListStudentIds;
