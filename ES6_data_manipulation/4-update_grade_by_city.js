c2f98e0f4dce8e612343d67b5cabbb3adc180833
jonseb974 <5151@holbertonstudents.com>
2023-03-28 21:46:22 +0200

    test script file 4

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,2 +1,13 @@
-function updateStudentGradeByCity(students) {
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  return students
+    .filter((student) => student.city === city)
+    .map((student) => {
+      const newGrade = newGrades.find((grade) => grade.studentID === student.id);
+      return {
+        id: student.id,
+        firstName: student.firstName,
+        location: student.location,
+        grade: newGrade ? newGrade.grade : 'N/A',
+      };
+    });
 }
