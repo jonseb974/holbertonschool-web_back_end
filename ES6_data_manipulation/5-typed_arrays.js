06f4cf1775604479e217b7fb54da49bee851cca0
jonseb974 <5151@holbertonstudents.com>
2023-03-28 22:12:35 +0200

    add scrit file5

---
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,13 @@
+export default function createInt8TypedArray(length, position, value) {
+  const buffer = new ArrayBuffer(length);
+  const int8View = new Int8Array(buffer);
+
+  if (position >= length || position < 0) {
+    throw Error('Position outside range');
+  }
+
+  int8View[position] = value;
+  return new DataView(buffer);
+}
+
+module.exports = createInt8TypedArray;
