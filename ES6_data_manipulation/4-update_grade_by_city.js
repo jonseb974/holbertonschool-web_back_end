c1e8a98e4d754e9f55aa3e9e8199d286a856dafd
jonseb974 <5151@holbertonstudents.com>
2023-03-28 21:56:05 +0200

    add sript v2 file4

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,12 +2,7 @@ export default function updateStudentGradeByCity(students, city, newGrades) {
   return students
     .filter((student) => student.city === city)
     .map((student) => {
-      const newGrade = newGrades.find((grade) => grade.studentID === student.id);
-      return {
-        id: student.id,
-        firstName: student.firstName,
-        location: student.location,
-        grade: newGrade ? newGrade.grade : 'N/A',
-      };
+      const findGrade = newGrades.find((newGrade) => newGrade.studentID === student.id);
+      return { ...student, grade: findGrade ? findGrade.grade : 'N/A' };
     });
 }
