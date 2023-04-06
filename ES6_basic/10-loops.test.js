360a7afcc9374652f8fe94c6694dcc0465f07e7c
jonseb974 <5151@holbertonstudents.com>
2023-04-06 11:30:28 +0200

    delete test_1file

--- ES6_basic/10-loops.test.js
+++
@@ -1,5 +0,0 @@
-import appendToEachArrayValue from './10-loops.js';
-
-test('returns the correct array', () => {
-  expect(appendToEachArrayValue(['appended', 'fixed', 'displayed'], 'correctly-')).toEqual(['correctly-appended', 'correctly-fixed', 'correctly-displayed']);
-});
