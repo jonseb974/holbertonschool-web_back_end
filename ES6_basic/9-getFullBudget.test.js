1fcf2e8fe7026070ba4cefe0352cdeee0ca903fe
jonseb974 <5151@holbertonstudents.com>
2023-04-06 11:41:30 +0200

    delete file9_test

--- ES6_basic/9-getFullBudget.test.js
+++
@@ -1,7 +0,0 @@
-import getFullBudgetObject from './9-getFullBudget.js';
-
-test('getFullBudgetObject returns the right object', () => {
-    const fullBudget = getFullBudgetObject(200, 500, 100);
-    expect(fullBudget.getIncomeInDollars(fullBudget.income)).toEqual('$200');
-    expect(fullBudget.getIncomeInEuros(fullBudget.income)).toEqual('200 euros');
-  });
