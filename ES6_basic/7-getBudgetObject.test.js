3cff0ffe5c4a251aa5754d099921e1028a1f185b
jonseb974 <5151@holbertonstudents.com>
2023-04-06 11:40:06 +0200

    delete file7_test

--- ES6_basic/7-getBudgetObject.test.js
+++
@@ -1,9 +0,0 @@
-import getBudgetObject from './7-getBudgetObject.js';
-
-test('getBudgetObject returns the right object', () => {
-  expect(getBudgetObject(200, 500, 100)).toEqual({
-    "capita": 100,
-    "gdp": 500,
-    "income": 200,
-  });
-});
